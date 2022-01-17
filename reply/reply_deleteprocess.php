02da99973ced4ff2927e3a858be5dc1dcb77d0ae
pleasesavemyearth <qkrwlgus0317@naver.com>
2022-01-17 09:41:55 +0900

    01-17-09:41

--- reply/reply_deleteprocess.php
+++ reply/reply_deleteprocess.php
@@ -23,8 +23,6 @@ if ($conn->query($sql) == TRUE) {
     echo outmsg(DELETE_FAIL);
   }
 
-// border tbl 에러남
-//$sql = "UPDATE border SET b_reply = b_reply - 1 WHERE id=".$col_num;
 $conn->query($sql);
 
 $conn->close();
